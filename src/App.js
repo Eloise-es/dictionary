3ff57eb8f704c2bdab27602d8bf3c36b05ccbec2
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-07-04 22:40:32 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo}></img>
+        </header>
+        <main>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
